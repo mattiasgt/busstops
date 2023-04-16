b869fc719914e6c55611a4cc71a32630bfb20b9c
mattiasgt <mattiasgt543@gmail.com>
2023-04-16 22:22:00 +0100

    Initial Spring Boot commit

---
+++ src/main/java/com/assignment/busstops/BusstopsApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.busstops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusstopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusstopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/busstops/BusstopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.busstops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusstopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
