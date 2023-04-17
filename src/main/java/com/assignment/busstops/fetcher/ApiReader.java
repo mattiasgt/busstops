f1d6dff32768249637d0ccc5c76cda39da40ec68
mattiasgt <mattiasgt543@gmail.com>
2023-04-17 17:33:38 +0100

    Add Spring Rest calls

--- src/main/java/com/assignment/busstops/fetcher/ApiReader.java
+++ src/main/java/com/assignment/busstops/fetcher/ApiReader.java
@@ -1,7 +1,26 @@
 package com.assignment.busstops.fetcher;
 
 import com.assignment.busstops.model.BusLine;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.function.Function;
 
-public interface ApiReader extends Function<String, BusLine>  {}
+import static java.util.Arrays.asList;
+
+public class ApiReader implements Function<String, BusLine> {
+    static final String baseURL = "mam";
+
+    @Override
+    public BusLine apply(String busLine) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = baseURL + busLine;
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        if(HttpStatus.OK.equals(response.getStatusCode())) {
+            return new BusLine(busLine, response.getBody());
+        } else {
+            return new BusLine("BusLine couldn't be fetched", asList());
+        }
+    }
+}
