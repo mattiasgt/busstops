1af9c488b9446944212d709a553c8a58e0f19cce
mattiasgt <mattiasgt543@gmail.com>
2023-04-17 17:33:12 +0100

    Add Selector

--- src/main/java/com/assignment/busstops/BusstopsApplication.java
+++ src/main/java/com/assignment/busstops/BusstopsApplication.java
@@ -1,10 +1,23 @@
 package com.assignment.busstops;
 
+import com.assignment.busstops.model.BusLine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+import static java.util.Comparator.*;
+
 @SpringBootApplication
 public class BusstopsApplication {
+	interface Selector extends UnaryOperator<Stream<BusLine>> {}
+
+	Comparator<BusLine> bySize = comparing(BusLine::size);
+
+	Function<Integer, Selector> selector = n -> busLines -> busLines.sorted(bySize).limit(n);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BusstopsApplication.class, args);
