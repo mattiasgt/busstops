e3af2f387d7041b98dfec0ffb8b095d8d2a5dab6
mattiasgt <mattiasgt543@gmail.com>
2023-04-17 17:31:57 +0100

    Refactor BusLine and BusStop

--- src/main/java/com/assignment/busstops/model/BusLine.java
+++ src/main/java/com/assignment/busstops/model/BusLine.java
@@ -2,4 +2,9 @@ package com.assignment.busstops.model;
 
 import java.util.List;
 
-public record BusLine(List<BusStop> stops) {}
+public record BusLine(String lineName, List<BusStop> stops) {
+    public record BusStop(String name) {}
+    public Integer size() {
+        return stops.size();
+    }
+}
--- src/main/java/com/assignment/busstops/model/BusStop.java
+++
@@ -1,3 +0,0 @@
-package com.assignment.busstops.model;
-
-public record BusStop(String name) {}
